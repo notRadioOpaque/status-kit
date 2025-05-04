91dbe22a529d9bda071d898d918803e2c6c50c1a
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-05-04 18:11:06 +0100

    Refactor main page layout to include Header, Footer, FAQS, and CommandGuide components; enhance content with Tag and updated messaging for improved user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,59 @@
-// export default function Home() {
-//   return (
-//     <div>this is the main page</div>
-//   )
-// }
+import {Icon} from "@iconify/react";
 
-import Image from "next/image";
-
-import ThemeToggle from "@/components/theme-toggle";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import FAQS from "@/components/faqs";
+import {Tag} from "@/components/tag";
+import CommandGuide from "@/components/command-guide";
 
 export default function Home() {
   return (
-    <div className="bg-noise grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 bg-white p-8 pb-20 text-[#171717] transition sm:p-20 dark:bg-[#171717] dark:text-white">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <ThemeToggle />
+    <div className="bg-noise relative grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 bg-white p-8 pb-20 text-[#171717] transition sm:p-10 dark:bg-[#171717] dark:text-white">
+      <Header />
 
+      <main className="row-start-2 flex h-max w-full max-w-7xl flex-col items-center justify-center gap-[32px] text-center">
+        <Tag
+          icon={
+            <Icon
+              className="!text-white dark:text-[#171717]"
+              height="48"
+              icon="icon-park:code-laptop"
+              width="48"
+            />
+          }
+          text="Decode the Web, One Status at a Time"
+          textColor="#171717"
+        />
+
+        <h1 className="text-4xl font-extrabold">
+          Explore HTTP status codes with clarity and speed
+        </h1>
+        <h1 className="mt-[-20px] text-4xl font-extrabold">no fluff, just the facts you need.</h1>
+
+        <p className="">
+          Whether you&apos;re debugging APIs or just curious, StatusKit makes HTTP codes easy to
+          understand.
+        </p>
+        <p className="mt-[-30px]">
+          Perfect for devs, learners, and curious minds — learn when, why, and how each code is
+          used.
+        </p>
+
+        <div className="flex h-24 w-full items-center justify-center border">
+          Search bar goes here
+        </div>
+
+        <CommandGuide />
+      </main>
+
+      <FAQS />
+      <Footer />
+    </div>
+  );
+}
+
+{
+  /* <main className="row-start-2 flex h-max w-full max-w-7xl flex-col items-center gap-[32px] border sm:items-start">
         <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
@@ -50,36 +90,5 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image aria-hidden alt="File icon" height={16} src="/file.svg" width={16} />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image aria-hidden alt="Window icon" height={16} src="/window.svg" width={16} />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image aria-hidden alt="Globe icon" height={16} src="/globe.svg" width={16} />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </main> */
 }

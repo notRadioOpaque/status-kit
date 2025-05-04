c86cb71670b4196e5ed6f79780a1b3cb19a09a7c
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-05-04 16:48:52 +0100

    Update Logo component to use Next.js Link and Iconify for enhanced branding and navigation

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+import {Icon} from "@iconify/react";
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Link className="flex items-center gap-3" href={"/"}>
+      <Icon height="48" icon="material-icon-theme:folder-tools-open" width="48" />
+
+      <p className="text-3xl font-bold">Status Kit</p>
+    </Link>
+  );
 };
 
 export default Logo;

5f2a97cb3c91de33cc55ffed681e6ae513a12528
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-05-24 17:23:53 +0100

    fixed bug on api

--- src/app/api/duck/[code]/route.ts
+++ src/app/api/duck/[code]/route.ts
@@ -1,7 +1,8 @@
 import {NextResponse} from "next/server";
 
 export async function GET(request: Request, {params}: {params: {code: string}}) {
-  const {code} = params;
+  const url = new URL(request.url);
+  const code = url.pathname.split("/").pop();
 
   try {
     const res = await fetch(`https://httpducks.com/${code}.json`);
